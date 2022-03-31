e2639a90da2c30621f109e815c29a006163451f4
João Freixo <48659801+Freixo19@users.noreply.github.com>
2022-03-31 15:11:16 +0100

    Add files via upload

--- writenoncanonical.c
+++ writenoncanonical.c
@@ -6,6 +6,9 @@
 #include <termios.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
 
 #define BAUDRATE B38400
 #define MODEMDEVICE "/dev/ttyS1"
@@ -21,12 +24,39 @@
 
 
 volatile int STOP=FALSE;
+int timeoutCounter = 0, flag = 0, fd;
+unsigned char buf[5];
+struct termios oldtio,newtio;
+
+
+void atende()                   // atende alarme
+{
+    if (timeoutCounter == 3)
+    {
+        //printf("Vou Fechar\n");
+        if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) 
+        {
+            perror("tcsetattr");
+            exit(-1);
+        }
+        close(fd);
+    } else
+    if(flag == 0)
+    {
+        timeoutCounter++;
+	    printf("TIMEOUT # %d\n", timeoutCounter);
+	    alarm(3);
+	    int res2 = write(fd,buf,5);   
+        //printf("%d bytes written\n", res2);
+	}
+}
+
+
 
 int main(int argc, char** argv)
 {
-    int fd,c, res;
-    struct termios oldtio,newtio;
-    unsigned char buf[5];
+    int c, res;
+    
     int i = 0, sum = 0, speed = 0;
     
     if ( (argc < 2) || 
@@ -65,7 +95,7 @@ int main(int argc, char** argv)
 
 
   /* 
-    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
+    VTIME e VMIN devem ser alterados de forma char_read proteger com um temporizador char_read 
     leitura do(s) pr�ximo(s) caracter(es)
   */
 
@@ -80,6 +110,7 @@ int main(int argc, char** argv)
 
     printf("New termios structure set\n");
 
+    (void) signal(SIGALRM, atende);
     
     buf[0] = FLAG;
     buf[1] = A_3;
@@ -93,11 +124,12 @@ int main(int argc, char** argv)
  
 
   /* 
-    O ciclo FOR e as instru��es seguintes devem ser alterados de modo a respeitar 
+    O ciclo FOR e as instru��es seguintes devem ser alterados de modo char_read respeitar 
     o indicado no gui�o 
   */
-    unsigned char a;
+    unsigned char char_read;
     int state = 0;
+    alarm(3);
     while (1) 
     {                 
               
@@ -105,62 +137,77 @@ int main(int argc, char** argv)
         printf("STOP\n");
         break;
       }
-      res = read(fd,&a,1);
-      printf("a= %d \n",a);
-      if(state==0 && a==FLAG){
+      if((res = read(fd,&char_read,1)) == -1)
+      {
+        printf("CONNECTION FAILED\n");
+        return -1;
+      }
+      
+      flag = 1;
+
+        
+      /*---------------------------------------------------------------------*/
+      /*-----------------------------STATE MACHINE---------------------------*/
+      /*---------------------------------------------------------------------*/
+      
+      printf("char_read= %d \n",char_read);
+      if(state==0 && char_read==FLAG){
         state=1;
         printf("State 0-1\n");
       }
-      else if(state==0 && a!=FLAG){
+      else if(state==0 && char_read!=FLAG){
         state=0;
         printf("State 0-0\n");
       }
-      else if(state==1 && a==A_1){
+      else if(state==1 && char_read==A_1){
         state=2;
         printf("State 1-2\n");
       }
-      else if(state==1 && a==FLAG){
+      else if(state==1 && char_read==FLAG){
         state=1;
         printf("State 1-1\n");
       }
-      else if(state==1 && a!=FLAG && a!=A_1){
+      else if(state==1 && char_read!=FLAG && char_read!=A_1){
         state=0;
         printf("State 1-0\n");
       }
-      else if(state==2 && a==FLAG){
+      else if(state==2 && char_read==FLAG){
         state=1;
         printf("State 2-1\n");
       }
-      else if(state==2 && a==C){
+      else if(state==2 && char_read==C){
         state=3;
         printf("State 2-3\n");
       }
-      else if(state==2 && a!=FLAG && a!=C){
+      else if(state==2 && char_read!=FLAG && char_read!=C){
         state=0;
         printf("State 2-0\n");
       }
-      else if(state==3 && a==FLAG){
+      else if(state==3 && char_read==FLAG){
         state=1;
         printf("State 3-1\n");
       }
-      else if(state==3 && a==BCC_1){
+      else if(state==3 && char_read==BCC_1){
         state=4;
         printf("State 3-4\n");
       }
-      else if(state==3 && a!=FLAG && a!=BCC_1){
+      else if(state==3 && char_read!=FLAG && char_read!=BCC_1){
         state=0;
         printf("State 3-0\n");
       }
-      else if(state==4 && a==FLAG){
+      else if(state==4 && char_read==FLAG){
         state=5;
         printf("State 4-5\n");
         
       }
-      else if(state==4 && a!=FLAG){
+      else if(state==4 && char_read!=FLAG){
         state=0;
         printf("State 4-0\n");
       }
       
+      /*---------------------------------------------------------------------*/
+      /*-----------------------------STATE MACHINE---------------------------*/
+      /*---------------------------------------------------------------------*/
     }
 
 
@@ -170,7 +217,7 @@ int main(int argc, char** argv)
       exit(-1);
     }
 
-
+    
 
 
     close(fd);
